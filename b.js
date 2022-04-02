8239320e31562e5458953c0f17b04f0e2e2426f6
maskriwil <102317169+maskriwil@users.noreply.github.com>
2022-04-02 11:07:19 +0700

    Create b.js

---
+++ b.js
@@ -0,0 +1,32 @@
+const userAgent = require('user-agents');
+const puppeteer = require('puppeteer');
+(async () => {
+    for(i=0;i<3000;i++){
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.setUserAgent(userAgent.toString())
+    await page.goto('https://codehs.com/sandbox/id/python-3-1pZqzQ')
+    await page.waitForTimeout(2000)
+    await page.mouse.click(580,125,{'button': 'left'})
+    await page.waitForTimeout(1000)
+    await page.mouse.click(580,125,{'button': 'left'})
+    await page.waitForTimeout(15000)
+    for(y=0;y<100;y++) {
+        await page.mouse.click(580,400,{'button': 'left'})
+        await page.keyboard.press('KeyS')
+        await page.waitForTimeout(5000)
+        await page.keyboard.press('KeyS')
+        await page.waitForTimeout(5000)
+        await page.keyboard.press('KeyH')
+        await page.waitForTimeout(5000)
+        console.log(`pimpo${i}${y}`)
+    }
+    await page.waitForTimeout(1000)
+    await page.mouse.click(630,125,{'button': 'left'})
+    await page.waitForTimeout(5000)
+    await browser.close();
+    }
+})();
